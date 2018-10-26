c20a776237676381b5e26d8e02af04ea5b144f93
jaojewe <43376277+jaojewe@users.noreply.github.com>
2018-10-26 17:36:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,4 +52,25 @@ message.guild.members.forEach(m => {
 });
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "say") {
+if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+message.channel.send(args.join("  "))
+    message.delete();
+  }
+
+
+
+});
+
+
 client.login(process.env.BOT_TOKEN);
